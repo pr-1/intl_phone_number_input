99cbebb892793b98ab6bc5f1a368234911003688
Prince <prince17.ps6@gmail.com>
2021-03-25 13:34:20 +0530

    internal use only

--- lib/src/utils/phone_number/phone_number_util.dart
+++ lib/src/utils/phone_number/phone_number_util.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:intl_phone_number_input/src/utils/phone_number.dart';
 import 'package:libphonenumber/libphonenumber.dart' as l;
-import 'package:libphonenumber_plugin/libphonenumber_plugin.dart' as p;
 
 /// A wrapper class [PhoneNumberUtil] that basically switch between plugin available for `Web` or `Android or IOS` and `Other platforms` when available.
 class PhoneNumberUtil {
@@ -9,9 +7,7 @@ class PhoneNumberUtil {
   /// Returns [Future<String>]
   static Future<String?> getNameForNumber(
       {required String phoneNumber, required String isoCode}) async {
-    return kIsWeb
-        ? p.PhoneNumberUtil.getNameForNumber(phoneNumber, isoCode)
-        : l.PhoneNumberUtil.getNameForNumber(
+    return l.PhoneNumberUtil.getNameForNumber(
             phoneNumber: phoneNumber, isoCode: isoCode);
   }
 
@@ -20,9 +16,7 @@ class PhoneNumberUtil {
   /// Returns [Future<bool>].
   static Future<bool?> isValidNumber(
       {required String? phoneNumber, required String? isoCode}) async {
-    return kIsWeb
-        ? p.PhoneNumberUtil.isValidNumber(phoneNumber!, isoCode!)
-        : l.PhoneNumberUtil.isValidPhoneNumber(
+    return l.PhoneNumberUtil.isValidPhoneNumber(
             phoneNumber: phoneNumber!, isoCode: isoCode!);
   }
 
@@ -31,9 +25,7 @@ class PhoneNumberUtil {
   /// Returns [Future<String>]
   static Future<String?> normalizePhoneNumber(
       {required String? phoneNumber, required String isoCode}) async {
-    return kIsWeb
-        ? p.PhoneNumberUtil.normalizePhoneNumber(phoneNumber!, isoCode)
-        : l.PhoneNumberUtil.normalizePhoneNumber(
+    return l.PhoneNumberUtil.normalizePhoneNumber(
             phoneNumber: phoneNumber!, isoCode: isoCode);
   }
 
@@ -42,9 +34,7 @@ class PhoneNumberUtil {
   static Future<RegionInfo> getRegionInfo(
       {required String? phoneNumber, required String isoCode}) async {
     var response;
-    response = kIsWeb
-        ? await p.PhoneNumberUtil.getRegionInfo(phoneNumber!, isoCode)
-        : await l.PhoneNumberUtil.getRegionInfo(
+    response = await l.PhoneNumberUtil.getRegionInfo(
             phoneNumber: phoneNumber!, isoCode: isoCode);
 
     return RegionInfo(
@@ -57,12 +47,11 @@ class PhoneNumberUtil {
   /// Returns [Future<PhoneNumberType>] type of phone number
   static Future<PhoneNumberType> getNumberType(
       {required String phoneNumber, required String isoCode}) async {
-    var webType = await p.PhoneNumberUtil.getNumberType(phoneNumber, isoCode);
     var mobileType = await l.PhoneNumberUtil.getNumberType(
         phoneNumber: phoneNumber, isoCode: isoCode);
 
     return PhoneNumberTypeUtil.getType(
-        kIsWeb ? webType.index : mobileType.index);
+        mobileType.index);
   }
 
   /// [formatAsYouType] uses Google's libphonenumber input format as you type.
@@ -70,9 +59,7 @@ class PhoneNumberUtil {
   /// Returns [Future<String>]
   static Future<String?> formatAsYouType(
       {required String? phoneNumber, required String? isoCode}) async {
-    return kIsWeb
-        ? p.PhoneNumberUtil.formatAsYouType(phoneNumber!, isoCode!)
-        : l.PhoneNumberUtil.formatAsYouType(
+    return l.PhoneNumberUtil.formatAsYouType(
             phoneNumber: phoneNumber!, isoCode: isoCode!);
   }
 }
